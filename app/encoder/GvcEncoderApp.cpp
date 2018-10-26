8288a47dba6e7d00365bb18263e34cac35c1a25e
RMontas <ricardojorgesmonteiro@gmail.com>
2018-10-26 17:15:17 +0100

    Combined GvcEncoderCfgApp class with GvcEncoderApp class.

--- app/encoder/GvcEncoderApp.cpp
+++ app/encoder/GvcEncoderApp.cpp
@@ -24,6 +24,8 @@
 
 #include "GvcEncoderApp.h"
 
+namespace po = df::program_options_lite;
+
 GvcEncoderApp::GvcEncoderApp()
 {
 	m_iFrameRcvd = 0;
@@ -32,6 +34,14 @@ GvcEncoderApp::GvcEncoderApp()
 
 GvcEncoderApp::~GvcEncoderApp()
 {
+}
+
+void GvcEncoderApp::create()
+{  //
+}
+
+void GvcEncoderApp::destroy()
+{  //
 }
 
 void GvcEncoderApp::encode()
@@ -130,6 +140,136 @@ void GvcEncoderApp::xDestroyLib()
 	//m_cGvcEnc.destroy(); TODO: Add to GvcEncoder
 }
 
+bool GvcEncoderApp::parseCfg( int argc, char* argv[] )
+{
+	bool do_help = true;
+	int warnUnknowParameter = 0;
+	int tmpChromaFormat = 0;
+	int tmpInternalBitDepth = 0;
 
+	po::Options opts;
+	opts.addOptions()
+			( "help", do_help, false, "this help text" )
+			( "c", po::parseConfigFile, "configuration file name" )
+			( "WarnUnknowParameter,w", warnUnknowParameter, 0, "warn for unknown configuration parameters instead of failing" )
+			( "InputFile,i", m_inputFileName, string( "" ), "Original YUV input file name" )
+			( "BitstreamFile,b", m_bitstreamFileName, string( "" ), "Bitstream output file name" )
+			( "ReconFile,o", m_reconFileName, string( "" ), "Reconstructed YUV output file name" )
+			( "SourceWidth,-wdt", m_iSourceWidth, 0, "Source picture width" )
+			( "SourceHeight,-hgt", m_iSourceHeight, 0, "Source picture height" )
+			( "QP,q", m_iQP, 30, "Qp value" )
+			("MaxCUWidth",                                      m_uiMaxCUWidth,                                     64u)
+			("MaxCUHeight",                                     m_uiMaxCUHeight,                                    64u)
+			("MaxPartitionDepth,h",                             m_uiMaxCUDepth,                                      4u, "CU depth")
+			("ChromaFormat",                               tmpChromaFormat,                               420, "ChromaFormat")
+			("FramesToBeEncoded,f",                             m_framesToBeEncoded,                                  0, "Number of frames to be encoded (default=all)")
+			("BitDepth",                                tmpInternalBitDepth,                8, "Bit-depth the codec operates at. (default:MSBExtendedBitDepth). If different to MSBExtendedBitDepth, source data will be converted");
+
+	po::setDefaults( opts );
+	po::ErrorReporter err;
+	const list<const char*>& argv_unhandled = po::scanArgv( opts, argc, (const char**)argv, err );
+
+	for( list<const char*>::const_iterator it = argv_unhandled.begin(); it != argv_unhandled.end(); it++ )
+	{
+		fprintf( stderr, "Unhandled argument ignored: `%s'\n", *it );
+	}
+
+	if( argc == 1 || do_help )
+	{
+		/* argc == 1: no options have been specified */
+		po::doHelp( cout, opts );
+		return false;
+	}
+
+	if( err.is_errored )
+	{
+		if( !warnUnknowParameter )
+		{
+			/* error report has already been printed on stderr */
+			return false;
+		}
+	}
+
+	m_chromaFormat = numberToChromaFormat(tmpChromaFormat);
+	m_bitDepth[CHANNEL_TYPE_LUMA] = 8;
+	m_bitDepth[CHANNEL_TYPE_CHROMA] = 8;
+	m_aiPad[1] = m_aiPad[0] = 0;
+
+	// check validity of input parameters
+	xCheckParameter();
+
+	// print-out parameters
+	xPrintParameter();
+
+	return true;
+}
+
+void GvcEncoderApp::xCheckParameter()
+{
+	bool check_failed = false; /* abort if there is a fatal configuration problem */
+#define xConfirmPara( a, b ) check_failed |= confirmPara( a, b )
+
+	xConfirmPara( m_bitstreamFileName.empty(), "A bitstream file name must be specified (BitstreamFile)" );
+	xConfirmPara( m_iQP < 0 || m_iQP > 51, "QP exceeds supported range (0 to 51)" );
+	xConfirmPara( ( m_iSourceWidth % 4 ) != 0, "Resulting coded frame width must be a multiple of the minimum CU size (4)" );
+	xConfirmPara( ( m_iSourceHeight % 4 ) != 0, "Resulting coded frame height must be a multiple of the minimum CU size (4)" );
+	xConfirmPara( (m_uiMaxCUWidth & (m_uiMaxCUWidth - 1)) != 0, "Max CU size must be a power of 2" );
+	xConfirmPara( (m_uiMaxCUHeight & (m_uiMaxCUHeight - 1)) != 0, "Max CU size must be a power of 2" );
+	xConfirmPara( m_chromaFormat == NUM_CHROMA_FORMAT, "Chroma format must be 400, 420, 422 or 444" );
+	xConfirmPara( m_framesToBeEncoded < 0, "Frame number must larger or equal to zero" );
+	xConfirmPara( m_bitDepth[CHANNEL_TYPE_LUMA] <= 0 && m_bitDepth[CHANNEL_TYPE_LUMA] > 16, "bit depth must be between 1 and 16" );
+
+#undef xConfirmPara
+	if( check_failed )
+	{
+		exit( EXIT_FAILURE );
+	}
+}
+
+void GvcEncoderApp::xPrintParameter()
+{
+	printf( "\n" );
+	printf( "Input          File                    : %s\n", m_inputFileName.c_str() );
+	printf( "Bitstream      File                    : %s\n", m_bitstreamFileName.c_str() );
+	printf( "Reconstruction File                    : %s\n", m_reconFileName.c_str() );
+	printf( "Resolution                             : %dx%d\n", m_iSourceWidth, m_iSourceHeight );
+	printf( "Number of frames                       : %d\n", m_framesToBeEncoded );
+	printf( "QP                                     : %d\n", m_iQP );
+	printf( "Max CU Width                           : %d\n", m_uiMaxCUWidth );
+	printf( "Max CU Height                          : %d\n", m_uiMaxCUHeight );
+	printf( "Max Partition Depth                    : %d\n", m_uiMaxCUDepth );
+	printf( "Chroma Format                          : %d\n", m_chromaFormat );
+	printf( "Bit Depth                              : %d\n", m_bitDepth[CHANNEL_TYPE_LUMA] );
+	printf( "\n\n" );
+
+	fflush( stdout );
+}
+
+void GvcEncoderApp::xPrintUsage()
+{
+}
+
+bool GvcEncoderApp::confirmPara( bool bflag, const char* message )
+{
+	if( !bflag )
+	{
+		return false;
+	}
+
+	printf( "Error: %s\n", message );
+	return true;
+}
+
+ChromaFormat GvcEncoderApp::numberToChromaFormat(int val)
+{
+	switch (val)
+	{
+		case 400: return CHROMA_400; break;
+		case 420: return CHROMA_420; break;
+		case 422: return CHROMA_422; break;
+		case 444: return CHROMA_444; break;
+		default:  return NUM_CHROMA_FORMAT;
+	}
+}
 
 
--- app/encoder/GvcEncoderApp.h
+++ app/encoder/GvcEncoderApp.h
@@ -25,13 +25,19 @@
 #ifndef __GVCENCODERAPP_H__
 #define __GVCENCODERAPP_H__
 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
 #include "GvcEncoder.h"
-#include "GvcEncoderCfgApp.h"
 #include "TVideoIOYuv.h"
 #include "GvcFrameUnitYuv.h"
+#include "program_options_lite.h"
+#include "TypeDef.h"
 
 /// encoder application class
-class GvcEncoderApp : public GvcEncoderCfgApp
+class GvcEncoderApp
 {
   private:
 	// class interface
@@ -43,28 +49,49 @@ class GvcEncoderApp : public GvcEncoderCfgApp
 	unsigned int m_totalBytes;
 
   protected:
+	// file I/O
+	std::string m_inputFileName;      ///< source file name
+	std::string m_bitstreamFileName;  ///< output bitstream file
+	std::string m_reconFileName;      ///< output reconstruction file
+	// source specification
+	int m_iSourceWidth;   ///< source width in pixel
+	int m_iSourceHeight;  ///< source height in pixel
+	int m_aiPad[2];                                       ///< number of padded pixels for width and height
+	int m_framesToBeEncoded;
+	ChromaFormat m_chromaFormat;
+	int       m_bitDepth   [MAX_NUM_CHANNEL_TYPE];         ///< bit-depth of input file
+	// coding quality
+	int m_iQP;  ///< QP value of key-picture
+	// coding unit (CU) definition
+	unsigned int      m_uiMaxCUWidth;                                   ///< max. CU width in pixel
+	unsigned int      m_uiMaxCUHeight;                                  ///< max. CU height in pixel
+	unsigned int      m_uiMaxCUDepth;                                   ///< max. CU depth (as specified by command line)
+	// internal member functions
+	void xCheckParameter();  ///< check validity of configuration values
+	void xPrintParameter();  ///< print configuration values
+	void xPrintUsage();      ///< print usage
+	bool confirmPara( bool bflag, const char* message );
+	ChromaFormat numberToChromaFormat(int val);
 	// initialization
 	void  xCreateLib        ();                               ///< create files & encoder class
 	void  xInitLibCfg       ();                               ///< initialize internal variables
 	void  xInitLib          ();					              ///< initialize encoder class
 	void  xDestroyLib       ();                               ///< destroy encoder class
-
 	/// obtain required buffers
 	void xGetBuffer(GvcFrameUnitYuv*& rpcFrameYuvRec);
-
 	/// delete allocated buffers
 	void  xDeleteBuffer     ();
-
 	// file I/O
 	void xWriteOutput(std::ostream& bitstreamFile, int iNumEncoded); ///< write bitstream to file
 	void printRateSummary();
-
   public:
 	GvcEncoderApp();
 	virtual ~GvcEncoderApp();
-
+	void create();                            ///< create option handling class
+	void destroy();                           ///< destroy option handling class
 	void encode();                                 ///< main encoding function
 	GvcEncoder& getGvcEnc() { return m_cGvcEnc; }  ///< return encoder class pointer reference
+	bool parseCfg( int argc, char* argv[] );  ///< parse configuration file to fill member variables
 };
 
 #endif  // __GVCENCODERAPP_H__
